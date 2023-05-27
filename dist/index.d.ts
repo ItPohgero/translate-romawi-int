94d16353cafed440135521d015fe34ea9e5e11d0
wahyuagusarifin <wahyu.agusarifin@ai.astra.co.id>
2023-05-27 18:20:36 +0700

    update ts

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare const IntToRoman: (num: number) => string;
+export declare const RomanToInt: (roman: string) => number;
--- index.ts
+++
@@ -1,4 +0,0 @@
-import { IntToRoman, RomanToInt } from "./dist/main";
-
-console.log(RomanToInt("XXIV"));
-console.log(IntToRoman(123));
